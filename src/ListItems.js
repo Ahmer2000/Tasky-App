da5bf848bb2006aaf47400ddf8ebc7b3aa9be5a2
Ahmer2000 <ahmersayyed2000@gmail.com>
2023-12-04 03:05:28 +0530

    final change

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,20 +1,33 @@
-import React  from "react";
-import './ListItems.css'
+import React from "react";
+import "./ListItems.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FlipMove from "react-flip-move";
 function ListItems(props) {
-    const items = props.items;
-    const listItems = items.map(item => {
-        return <div className="list" key ={item.key}>
-            <p>{item.text} <span>
-                <FontAwesomeIcon className="faicons" icon='trash' onClick={() =>{
-                    props.deleteItem(item.key)
-                }}/>
-            </span></p>
-           
-        </div>
-    })
-    return(
-        <div>{listItems}</div>
-    )
+  const items = props.items;
+  const listItems = items.map((item) => {
+    return (
+        <FlipMove>
+      <div className="list" key={item.key}>
+        <p>
+          <input type="text" id={item.text} value={item.text} onChange={
+            (e) => {
+                props.setUpdate(e.target.value,item.key)
+            }
+          } />
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => {
+                props.deleteItem(item.key);
+              }}
+            />
+          </span>
+        </p>
+      </div>
+      </FlipMove>
+    );
+  });
+  return <div>{listItems}</div>;
 }
-export default ListItems
+export default ListItems;
